be59e75f6b171b3ffb4a68d095d15d46d9c46f6a
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 11:27:03 -0300

    verifica se estuda no SENAI

--- atividade 7.cs
+++ atividade 7.cs
@@ -10,16 +10,17 @@ namespace Verifica_numero
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Quantos produtos foram comprados?");
-            int quant = Convert.ToInt32(Console.ReadLine());
-            if ( quant > 100 ) 
-            {
-                Console.WriteLine("Qual o preço?");
-            Double preco = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("O novo preço com desconto de 12,5% é " + (preco * 0.875));
+            Console.WriteLine("Qual sua idade?");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual o nome da sua escola?");
+            string escola =Console.ReadLine().ToUpper;
+            Console.WriteLine("O novo preço com desconto de 12,5% é " + (preco * 0.875));
+            if (idade <=18 && escola == "SENAI" ) 
+            {         
+                Console.Write("O aluno é estudante do curso técnico em desenvolvimento de sistemas")
             }else
             {
-                Console.WriteLine("Não tem desconto");
+                Console.WriteLine("O aluno não é estudante do Sesi/Senai);
             }        
           
             Console.WriteLine("Aperte enter para finalizar");
