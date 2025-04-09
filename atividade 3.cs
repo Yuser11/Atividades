ab07b1a15a6cfa547f46cccc76ea4428df0623fe
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 10:47:25 -0300

    verificação de senha

---
+++ atividade 3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a senha");
+            int senha = Convert.ToInt32(Console.ReadLine());
+            if (senha == 56789) 
+            {
+                Console.WriteLine("Acesso permitido");
+            }else
+            {
+                Console.WriteLine("Acesso negado");
+            }
+            Console.WriteLine("Aperte enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
