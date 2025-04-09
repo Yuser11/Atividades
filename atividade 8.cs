3b403f1e011e18e109b7f84e7d93bfcbb23edb22
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 11:37:22 -0300

    operaçoes com um valor

---
+++ atividade 8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            if (valor > 0) 
+            {
+                Console.WriteLine("Escreva mais 2 numeros inteiros");
+                int valor1 = Convert.ToInt32(Console.ReadLine());
+                int valor2 = Convert.ToInt32(Console.ReadLine());
+                int resultado = valor * valor1 * valor2;
+                if (resultado > 45) 
+                {
+                    resultado++; resultado++; resultado++; resultado++; resultado++;
+                    Console.WriteLine("O resultado, adicionando 5, é " + resultado);
+                }
+                Console.WriteLine("Aperte enter para finalizar");
+                Console.Read();
+
+            }
+        }
+    }
+}
