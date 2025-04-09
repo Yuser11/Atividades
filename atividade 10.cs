8252adcef952b87ecab09e82b60e04c2180994c7
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 12:18:14 -0300

    vogal e consoante

---
+++ atividade 10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma letra");
+            char letra = (char)Console.Read();      
+                if (letra == 'a'||letra =='e'|| letra == 'i' || letra == 'o'||letra == 'u'|| letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') 
+                {
+                    Console.WriteLine("É uma vogal");
+                }
+                else
+                {
+                    Console.WriteLine("É uma consoante");
+                }                        
+            Console.WriteLine("Aperte enter para finalizar");
+            Console.Read();            
+        }
+    }
+}
