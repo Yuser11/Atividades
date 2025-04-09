663320b8aff6b8174366477538a1598841bdc0a5
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 12:26:14 -0300

    Seculo XXI

---
+++ atividade 12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um ano");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            if (ano >= 2001 && ano <=2100) 
+            {
+                Console.WriteLine("Pertence ao seculo XXI");
+            }else
+            {
+                Console.WriteLine("Nao pertence ao seculo XXI");
+            }
+            Console.WriteLine("Aperte enter para finalizar");
+            Console.Read();
+        }
+    }
+}
