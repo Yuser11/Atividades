331bd64f138739ee67e8a73406a1c835edced90a
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 10:41:44 -0300

    numero positivo

---
+++ atividade 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace verifica_positivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double num1 = 14.1;
+            Console.Write("O numero "+ num1);
+            if (num1 > 0)
+            {
+                Console.WriteLine(" é positivo");
+            }
+            else if (num1 < 0)
+            {
+                Console.WriteLine(" é negativo");
+            }
+            else
+            {
+                Console.WriteLine(" é neutro");
+            }
+            Console.WriteLine("Aperte enter para finalizar");
+            Console.Read();
+        }
+    }
+}
