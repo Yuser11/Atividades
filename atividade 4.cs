55b45564e8c50fedaf18ede5d1dce7b8e384b225
Yuri Momesso de Lima <yuri.momessodelima@gmail.com>
2025-04-09 10:49:50 -0300

    idade para votar

---
+++ atividade 4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a sua idade");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            if (idade > 16) 
+            {
+                Console.WriteLine("Você pode votar");
+            }else
+            {
+                Console.WriteLine("Você não pode votar");
+            }
+            Console.WriteLine("Aperte enter para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
